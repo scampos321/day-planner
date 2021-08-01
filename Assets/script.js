a544452d6546e3c6bfcddbb247eb02bd1d9c068c
scampos321 <sebastian.campos321@gmail.com>
2021-08-01 11:35:01 -0400

    added description array

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,43 @@ window.setInterval(function() {
     $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }, 1000);
 
+var containerArray = [
+{
+    hour: '9:00AM',
+    description: ''
+}, 
+{
+    hour: '10:00AM',
+    description: ''
+},
+{
+    hour: '11:00AM',
+    description: ''
+},
+{
+    hour: '12:00PM',
+    description: ''
+},
+{
+    hour: '1:00PM',
+    description: ''
+},
+{
+    hour: '2:00PM',
+    description: ''
+}, 
+{
+    hour: '3:00PM',
+    description: ''
+},
+{
+    hour: '4:00PM',
+    description: ''
+},
+{
+    hour: '5:00PM',
+    description: ''
+}
+]
+
+var storedData
